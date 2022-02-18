974d999d222cf3cd51eb43a407433a52d0825ed4
Ilias Achahbar <ilias.achahbar@student.ucll.be>
2022-02-18 20:23:09 +0100

    initial commit

---
+++ src/components/Navigation.vue
@@ -0,0 +1,45 @@
+<template>
+<div>
+  <div class="py-6 animate-moveintobottom bg-background fixed w-full top-0 left-0">
+    <div class="container flex justify-between items-center mx-auto px-8 md:px-14 lg:px-24 w-full text-white">
+      <div class="text-lg font-bold "><router-link :to="{ name: 'Home'}">Cremke'26</router-link></div>
+      <div class="hidden md:flex space-x-12 items-center text-md">
+        <router-link class="hover:text-green-500" :to="{ name: 'Bio'}">Menu</router-link>
+        <router-link class="hover:text-green-500" :to="{ name: 'Bio'}">Openingsuren</router-link>
+        <button type="button" class=" bg-green-400 hover:bg-transparent border border-transparent hover:border-green-300 font-bold rounded-sm text-sm p-2.5 text-center inline-flex items-center mr-2">
+  <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+  Contact
+    </button>
+      </div>
+      <button v-on:click="toggleMenu" class="md:hidden flex flex-col space-y-1 w-8">
+        <span class="bg-green-400 w-full h-1"></span>
+        <span class="bg-green-400 w-full h-1"></span>
+        <span class="bg-green-400 w-full h-1"></span>
+      </button>
+    </div>
+
+  </div>
+      <div v-if="showMenu" class="menu md:hidden fixed top-20 w-full space-y-4 flex flex-col bg-gray-200 items-center p-4">
+        <router-link class="hover:text-green-500" :to="{ name: 'Menu'}">Menu</router-link>
+        <button type="button" class=" bg-green-400 hover:bg-transparent border border-transparent hover:border-green-300 font-bold rounded-sm text-sm p-2.5 text-center inline-flex items-center mr-2">
+  <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+  Contact Me
+    </button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      showMenu: false,
+    };
+  },
+
+  methods: {
+    toggleMenu() {
+      this.showMenu = !this.showMenu;
+    },
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,78 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div>
+
+  <!-- hero -->
+  <section class="hero w-full bg-cones flex flex-col items-center justify-center mt-40 bg-background bg-blend-soft-light">
+    <div class="grid grid-cols-1 gap-8 text-white">
+      <h1 class="text-bold lg:text-9xl sm:text-8xl text-5xl">Cremke 26</h1>
+      <div class="flex md:flex-row flex-col justify-between gap-8 mt-20 w-full">
+        <button class="bg-green-400 p-4 rounded-sm w-full font-bold text-xl">Menu</button>
+        <button class="bg-green-400 p-4 rounded-sm w-full font-bold text-xl">Openingsuren</button>
+      </div>
+    </div>
+  </section>
+  <!-- end hero -->
+
+  <!-- menu -->
+  <section class="container mx-auto px-8 md:px-14 lg:px-24 w-full mt-24">
+    <div class="flex flex-col text-white border border-gray-400">
+      <h2 class="bg-background py-8 text-4xl">Menu</h2>
+      <div class="flex py-4 justify-around text-black bg-background text-white text-xl">
+        <h3>Smaak</h3>
+        <h3>Prijs</h3>
+      </div>
+      <div class="grid grid-cols-2 text-black border-b border-green-400 p-2.5 my-2" v-for="smaak in smaken" :key="smaak.id">
+        <p>{{smaak.name}}</p>
+        <p>{{smaak.price}}</p>
+      </div>
+    </div>
+  </section>
+  <!-- end menu -->
+
+  <!-- locatie -->
+  <section class="container mx-auto px-8 md:px-14 lg:px-24 w-full mt-24">
+    <div class="grid xl:grid-cols-2 grid-cols-1 gap-8">
+      <div class="">
+        <h2 class="bg-background py-8 text-4xl text-white">Locatie</h2>
+                <article class="flex justify-center items-center p-8 text-black">
+          Kopstraat 1, Tienen (3300)
+        </article>
+          <iframe class="w-full" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2520.471651804747!2d4.974966115906223!3d50.82242706837278!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c16c9e0f4b88ef%3A0x13b6cc0ca6a6ccf0!2sKopstraat%201%2C%203300%20Tienen!5e0!3m2!1sen!2sbe!4v1645210065956!5m2!1sen!2sbe" width="800" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+      </div>
+
+      <div class="flex flex-col">
+        <h2 class="bg-background py-8 text-4xl text-white">Openingsuren</h2>
+          <ul class="flex flex-col gap-4 mt-20">
+            <li>Maandag: 9:00u - 17:00u</li>
+            <li>Dinsdag: 9:00u - 17:00u</li>
+            <li>Woensdag: 9:00u - 17:00u</li>
+            <li>Donderdag: 9:00u - 17:00u</li>
+            <li>Vrijdag: 9:00u - 17:00u</li>
+            <li>Zaterdag: 9:00u - 17:00u</li>
+            <li>Zondag: gesloten</li>
+        </ul>
+      </div>
+    </div>
+  </section>
+  <!-- end locatie -->
+
+
+</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  name: 'Hero',
+  data() {
+    return {
+      smaken: [{"name": "vanille", "price": "2.50"}, {"name": "chocolade", "price": "2.50"}, {"name": "mokka", "price": "2.50"}, {"name": "citroen", "price": "2.50"}, {"name": "meloen", "price": "2.50"}, {"name": "rode vruchten", "price": "2.50"}]
+    };
+  },
 }
 </script>
+
+<style>
+.hero {
+  height: 40rem;
+}
+</style>
